b27fd4e834911a900f5946383e032c9a321745a3
jmwgop@gmail.com <jmwgop@gmail.com>
2025-05-21 00:07:23 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,12 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
